faaafb398f32beaf8ae6a4e8b6134e5690ea9466
bijaydas <imbijaydas@gmail.com>
2023-01-18 18:49:21 +0530

    chore: helper method added

--- doxcli/utils.py
+++ doxcli/utils.py
@@ -31,3 +31,26 @@ def read(path):
     if not os.path.isfile(path):
         raise RuntimeError(f'{path} not found')
     return open(path, 'r').read()
+
+
+def is_file(value):
+    return value is None or 'content' in value
+
+
+def has_content(value):
+    return value is not None and 'content' in value
+
+
+def create_file(path, content=""):
+    with open(path, 'w', encoding='utf8') as f:
+        f.write(content)
+
+
+def is_empty(value):
+    if isinstance(value, dict):
+        return bool(value) is False
+    return False
+
+
+def is_dir_empty(path):
+    return len(os.listdir(path)) == 0
