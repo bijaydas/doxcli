08e08220abf987bb38956f26a8baca0ecf3b67bc
bijaydas <me@bijaydas.com>
2023-09-02 21:20:09 +0530

    feat: godaddy service added

--- setup.py
+++ setup.py
@@ -19,7 +19,39 @@ def find_version():
 
 
 install_requires = [
-    'PyYAML==6.0',
+    'bleach==6.0.0',
+    'build==1.0.0',
+    'certifi==2023.7.22',
+    'cffi==1.15.1',
+    'charset-normalizer==3.2.0',
+    'cryptography==41.0.3',
+    'docutils==0.20.1',
+    'idna==3.4',
+    'importlib-metadata==6.8.0',
+    'jaraco.classes==3.3.0',
+    'jeepney==0.8.0',
+    'keyring==24.2.0',
+    'markdown-it-py==3.0.0',
+    'mdurl==0.1.2',
+    'more-itertools==10.1.0',
+    'packaging==23.1',
+    'pkginfo==1.9.6',
+    'pycparser==2.21',
+    'Pygments==2.16.1',
+    'pyproject_hooks==1.0.0',
+    'PyYAML==6.0.1',
+    'readme-renderer==41.0',
+    'requests==2.31.0',
+    'requests-toolbelt==1.0.0',
+    'rfc3986==2.0.0',
+    'rich==13.5.2',
+    'SecretStorage==3.3.3',
+    'six==1.16.0',
+    'tomli==2.0.1',
+    'twine==4.0.2',
+    'urllib3==2.0.4',
+    'webencodings==0.5.1',
+    'zipp==3.16.2',
 ]
 
 setup_options = dict(
@@ -32,15 +64,15 @@ setup_options = dict(
     author_email='me@bijaydas.com',
     url='https://github.com/bijaydas/doxcli',
     scripts=['cli.py'],
-    package_data={'doxcli': ['data/*']},
     install_requires=install_requires,
+    package_dir={'doxcli': 'doxcli'},
     entry_points='''
         [console_scripts]
         dox=cli:main
     ''',
     license='MIT License',
     extras_require={},
-    python_requires='>=3.7',
+    python_requires='>=3.0',
     project_urls={
         'Source': 'https://github.com/bijaydas/doxcli',
     },
